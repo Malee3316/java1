61116c4a2be9d3ffd1d913eb2d80d75e3ca266fd
Malee3316 <malenamurcia1235@gmail.com>
2022-05-17 15:56:38 -0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import org.w3c.dom.ls.LSOutput;
+
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println("Hello world!");
+
+
+                String queEs = "Perro";
+                String nombre = "Manchitas";
+                int edad = 2;
+                double comida = 1.5;
+                String pregunta = "wou";
+
+                System.out.println("Mi nombre es " + nombre + " tengo " + edad + " y soy " + queEs + " como " + comida + "por dia y la respuesta a la pregunta que me hicieron es" + pregunta);
+
+                queEs = "Pez";
+                nombre = "Nemo";
+                edad = 1;
+                comida = 0.140;
+                pregunta = "gluc";
+
+                System.out.println("Mi nombre es " + nombre + " tengo " + edad + " y soy " + queEs + " como " + comida + "por dia y la respuesta a la pregunta que me hicieron es" + pregunta);
+
+                queEs = "Gato";
+                nombre = "Silvestre";
+                edad = 3;
+                comida = 0.5;
+                pregunta = "miau";
+
+                System.out.println("Mi nombre es " + nombre + " tengo " + edad + " y soy " + queEs + " como " + comida + "por dia y la respuesta a la pregunta que me hicieron es" + pregunta);
+
+                queEs = "Tortuga";
+                nombre = "Manuelita";
+                edad = 12;
+                comida = 0.3;
+                pregunta = "peguajo";
+
+                System.out.println("Mi nombre es " + nombre + " tengo " + edad + " y soy " + queEs + " como " + comida + "por dia y la respuesta a la pregunta que me hicieron es" + pregunta);
+
+                queEs = "Pajarito";
+                nombre = "Gardel";
+                edad = 1;
+                comida = 0.350;
+                pregunta = "peguajo";
+
+                System.out.println("Mi nombre es " + nombre + " tengo " + edad + " y soy " + queEs +" como " + comida + "por dia y la respuesta a la pregunta que me hicieron es" + pregunta);
+
+
+            }
+        }
+
+
+
